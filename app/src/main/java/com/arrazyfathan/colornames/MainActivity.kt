60b6be2b8125a27ccfe07d5ca4f669ef53aa72b3
arrazyfathan <arrazy.rabbani266@gmail.com>
2022-12-08 16:30:50 +0700

    add feature color names

--- app/src/main/java/com/arrazyfathan/colornames/MainActivity.kt
+++ app/src/main/java/com/arrazyfathan/colornames/MainActivity.kt
@@ -1,18 +1,111 @@
 package com.arrazyfathan.colornames
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.lifecycle.Observer
 import com.arrazyfathan.colornames.databinding.ActivityMainBinding
+import com.arrazyfathan.colornames.networking.ColorApi
+import com.jakewharton.rxbinding4.view.clicks
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private val disposable = CompositeDisposable()
+
+    private val viewModel: MainViewModel by viewModels {
+        createWithFactory {
+            MainViewModel(
+                backgroundScheduler = Schedulers.io(),
+                mainScheduler = AndroidSchedulers.mainThread(),
+                colorCoordinator = ColorCoordinator(),
+                colorApi = ColorApi
+            )
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+        observe()
+    }
+
+    private fun observe() {
+        viewModel.hexStringLiveData.observe(
+            this,
+            Observer {
+                binding.hex.text = it
+            }
+        )
+
+        viewModel.rgbStringLiveData.observe(
+            this,
+            Observer {
+                binding.rgb.text = it
+            }
+        )
+        viewModel.colorNameLiveData.observe(
+            this,
+            Observer {
+                binding.colorName.text = it
+            }
+        )
+        viewModel.backgroundColorLiveData.observe(
+            this,
+            Observer {
+                animateColorChange(it)
+            }
+        )
+    }
+
+    private fun initUI() = with(binding) {
+        clear.clicks().subscribe { viewModel.clearClicked() }
+            .addTo(disposable)
+
+        back.clicks().subscribe { viewModel.backClicked() }
+            .addTo(disposable)
+
+        val digits =
+            listOf(zero, one, two, three, four, five, six, seven, eight, nine, A, B, C, D, E, F)
+                .map { digit ->
+                    digit.clicks().map { digit.text.toString() }
+                }
+
+        val digitStream = Observable.merge(digits)
+
+        digitStream.subscribe(viewModel::digitClicked)
+            .addTo(disposable)
+
+        /*colorName.clicks()
+            .subscribe {
+                val bottomSheetDialog = ColorBottomSheet.newInstance(hex.text.toString())
+                bottomSheetDialog.show(supportFragmentManager, "Custom Bottom Sheet Dialog")
+            }
+            .addTo(disposable)*/
+    }
+
+    private fun animateColorChange(newColor: Int) {
+        val colorFrom = binding.rootLayout.background as ColorDrawable
+        colorAnimator(colorFrom.color, newColor)
+            .subscribe { color ->
+                binding.rootLayout.setBackgroundColor(color)
+                window.statusBarColor = color
+            }
+            .addTo(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.clear()
     }
 }
---
+++ app/src/main/java/com/arrazyfathan/colornames/MainViewModel.kt
@@ -0,0 +1,123 @@
+package com.arrazyfathan.colornames
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.arrazyfathan.colornames.networking.ColorApi
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+/**
+ * Created by Ar Razy Fathan Rabbani on 08/12/22.
+ */
+class MainViewModel(
+    backgroundScheduler: Scheduler,
+    mainScheduler: Scheduler,
+    colorCoordinator: ColorCoordinator,
+    colorApi: ColorApi
+) : ViewModel() {
+
+    private val disposable = CompositeDisposable()
+
+    val hexStringSubject = BehaviorSubject.createDefault("#")
+    val hexStringLiveData = MutableLiveData<String>()
+    val backgroundColorLiveData = MutableLiveData<Int>()
+    val rgbStringLiveData = MutableLiveData<String>()
+    val colorNameLiveData = MutableLiveData<String>()
+    private val clearStream = PublishSubject.create<Unit>()
+    private val backStream = PublishSubject.create<Unit>()
+    private val digitStream = BehaviorSubject.create<String>()
+
+    init {
+        hexStringSubject
+            .subscribeOn(backgroundScheduler)
+            .observeOn(mainScheduler)
+            .subscribe(hexStringLiveData::postValue)
+            .addTo(disposable)
+
+        hexStringSubject
+            .subscribeOn(backgroundScheduler)
+            .observeOn(mainScheduler)
+            .map { if (it.length < 7) "#000000" else it }
+            .map { colorCoordinator.parseColor(it) }
+            .subscribe(backgroundColorLiveData::postValue)
+            .addTo(disposable)
+
+        hexStringSubject
+            .subscribeOn(backgroundScheduler)
+            .observeOn(mainScheduler)
+            .filter { it.length < 7 }
+            .map { "--" }
+            .subscribe(colorNameLiveData::postValue)
+            .addTo(disposable)
+
+        hexStringSubject
+            .filter {
+                it.length == 7
+            }
+            .observeOn(mainScheduler)
+            .flatMapSingle {
+                colorApi.getClosestColor(it)
+                    .subscribeOn(backgroundScheduler)
+            }
+            .map {
+                it.name.value
+            }
+            .subscribe(colorNameLiveData::postValue)
+            .addTo(disposable)
+
+        hexStringSubject
+            .subscribeOn(backgroundScheduler)
+            .observeOn(mainScheduler)
+            .map {
+                if (it.length == 7) {
+                    colorCoordinator.parseRgbColor(it)
+                } else {
+                    RGBColor(255, 255, 255)
+                }
+            }
+            .map { "${it.red},${it.green},${it.blue}" }
+            .subscribe(rgbStringLiveData::postValue)
+            .addTo(disposable)
+
+        clearStream
+            .map { "#" }
+            .subscribe(hexStringSubject::onNext)
+            .addTo(disposable)
+
+        backStream
+            .map { currentHexValue() }
+            .filter {
+                it.length >= 2
+            }
+            .map {
+                it.substring(0, currentHexValue().lastIndex)
+            }
+            .subscribe(hexStringSubject::onNext)
+            .addTo(disposable)
+
+        digitStream
+            .map { it to currentHexValue() }
+            .filter { it.second.length < 7 }
+            .map { it.second + it.first }
+            .subscribe(hexStringSubject::onNext)
+            .addTo(disposable)
+    }
+
+    private fun currentHexValue(): String {
+        return hexStringSubject.value ?: ""
+    }
+
+    fun backClicked() = backStream.onNext(Unit)
+
+    fun clearClicked() = clearStream.onNext(Unit)
+
+    fun digitClicked(digit: String) = digitStream.onNext(digit)
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+}
